fb669b109a208c2451de62a5f1be342dd5fa07d0
Ibrahim-official <144038542+Ibrahim-official@users.noreply.github.com>
2025-02-21 17:48:54 +0500

    Episode 8

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+use App\Models\Job;
 
 Route::get('/', function () {
     return view('home', [
@@ -10,25 +10,7 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-    return view('jobs', [
-        'jobs' => [
-            [
-                'id'    => 1,
-                'title' => 'Director',
-                'salary' => '$50,000'
-            ],
-            [
-                'id'    => 2,
-                'title' => 'Programmer',
-                'salary' => '$10,000'
-            ],
-            [
-                'id'    => 3,
-                'title' => 'Teacher',
-                'salary' => '$40,000'
-            ]
-        ]
-    ]);
+    return view('jobs', ['jobs' => Job::all()]);
 });
 
 Route::get('/contact', function () {
@@ -36,25 +18,7 @@ Route::get('/contact', function () {
 });
 
 Route::get('/job/{id}', function($id) {
-    $jobs = [
-                [
-                    'id'    => 1,
-                    'title' => 'Director',
-                    'salary' => '$50,000'
-                ],
-                [
-                    'id'    => 2,
-                    'title' => 'Programmer',
-                    'salary' => '$10,000'
-                ],
-                [
-                    'id'    => 3,
-                    'title' => 'Teacher',
-                    'salary' => '$40,000'
-                ]
-            ];
-
-    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+    $job = Job::find($id);
     
     return view('job', ['job' => $job]);
 });
